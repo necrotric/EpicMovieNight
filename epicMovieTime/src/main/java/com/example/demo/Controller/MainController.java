482d27b013469e234dee343a5222921c6152a3a5
Rickard Lundquist <rickard-lundquist@hotmail.com>
2018-12-13 11:23:27 +0100

    fixed conflicts

--- epicMovieTime/src/main/java/com/example/demo/Controller/MainController.java
+++ epicMovieTime/src/main/java/com/example/demo/Controller/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 @RestController
@@ -17,26 +18,35 @@ public class MainController {
     private Movies getFromDB;
     private Movies movies;
 
-        @GetMapping("/title")
-        public List<Search> search(@RequestParam String title ) {
-            RestTemplate restTemplate = new RestTemplate();
-       List<Search> search = Collections.singletonList(restTemplate.getForObject(
-             "http://www.omdbapi.com/?s="+ title + "&apikey=ea1db5cc", Search.class));
+    @GetMapping("/title")
+    public ArrayList<Movies> search(@RequestParam String title ) {
+        int counter=0;
+        ArrayList<Movies> movies = new ArrayList<>() ;
+        RestTemplate restTemplate = new RestTemplate();
+        List<Search> newSearch = Collections.singletonList(restTemplate.getForObject(
+                "http://www.omdbapi.com/?s="+ title + "&apikey=ea1db5cc", Search.class));
+        for (Search s: newSearch) {
+            for(int i = 0; i<s.getSearch().size(); i++){
+                Movies movie = new Movies();
+                movies.add(movie);
+                movies.get(i).setTitle(s.getSearch().get(i).getTitle());
+                movies.get(i).setImdbID(s.getSearch().get(i).getImdbID());
 
-            return search ;
-        }
-        @GetMapping("/title/movie")
-        public Movies movies(@RequestParam String imdb){
-            RestTemplate restTemplate = new RestTemplate();
-         getFromDB = repository.findMoviesByimdbID(imdb);
-            System.out.println(getFromDB);
-            if(getFromDB == null) {
-                movies = restTemplate.getForObject(
-                        "http://www.omdbapi.com/?i=" + imdb + "&apikey=ea1db5cc", Movies.class);
+            }}
+        return movies ;
+    }
+    @GetMapping("/title/movie")
+    public Movies movies(@RequestParam String imdb){
+        RestTemplate restTemplate = new RestTemplate();
+        getFromDB = repository.findMoviesByimdbID(imdb);
+        System.out.println(getFromDB);
+        if(getFromDB == null) {
+            movies = restTemplate.getForObject(
+                    "http://www.omdbapi.com/?i=" + imdb + "&apikey=ea1db5cc", Movies.class);
 
-                repository.save(movies);
-                return movies;
-            }
-                return getFromDB;
+            repository.save(movies);
+            return movies;
         }
+        return getFromDB;
+    }
 }
