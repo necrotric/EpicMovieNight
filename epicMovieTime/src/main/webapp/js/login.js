7bea71ec2f5230e7006ddce5b78a97fdc4abb92c
Rickard Lundquist <rickard-lundquist@hotmail.com>
2018-12-14 15:29:07 +0100

    autosearch implemented with api search and view, login timed restriction

--- epicMovieTime/src/main/webapp/js/login.js
+++ epicMovieTime/src/main/webapp/js/login.js
@@ -1,21 +1,51 @@
-function signOut() {
-    let auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
+// function signOut() {
+//     let auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function () {
+//         console.log('User signed out.');
+//     });
+// }
+//
+// function onSignIn(googleUser) {
+//     // Useful data for your client-side scripts:
+//     let profile = googleUser.getBasicProfile();
+//     console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+//     console.log('Full Name: ' + profile.getName());
+//     console.log('Given Name: ' + profile.getGivenName());
+//     console.log('Family Name: ' + profile.getFamilyName());
+//     console.log("Image URL: " + profile.getImageUrl());
+//     console.log("Email: " + profile.getEmail());
+//
+//     // The ID token you need to pass to your backend:
+//     let id_token = googleUser.getAuthResponse().id_token;
+//     console.log("ID Token: " + id_token);
+// };
+
+let attempt = 3;
+function validate(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if ( username === "user" && password === "user"){
+        window.open("/main.html"); // Redirecting to other page.
+        return false;
+    }
+    else{
+        attempt --;
+        alert("You have "+attempt+" attempts left");
+    }
+    if (attempt === 0) {
+        alert("To many attempts, try again in 60 seconds");
+        setTimeout(unlock,60000)
+        document.getElementById("username").disabled = true;
+        document.getElementById("password").disabled = true;
+        document.getElementById("loginBt").disabled = true;
+        return false;
+    }
 }
 
-function onSignIn(googleUser) {
-    // Useful data for your client-side scripts:
-    let profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-    // The ID token you need to pass to your backend:
-    let id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-};
+function unlock () {
+        attempt = 1;
+        document.getElementById("username").disabled = false;
+        document.getElementById("password").disabled = false;
+        document.getElementById("submit").disabled = false;
+        return false;
+}
--- epicMovieTime/src/main/webapp/js/search.js
+++ epicMovieTime/src/main/webapp/js/search.js
@@ -1,104 +1,32 @@
-/*function userSearch() {
-    $(document).ready(function () {
-        $.ajax({cache: false});
-        $('#userSearch').keyup(function () {
-            $('#result').html('');
-            $('#state').val('');
-            let searchField = $('#userSearch').val();
-            let expression = new RegExp(searchField, "i");
-            $.getJSON('recipes.json', function (data) {
-                $.each(data, function (key, value) {
-                    if (value.name.search(expression) !== -1 || value.categories.search(expression) !== -1 || value.description.search(expression) !== -1) {
-                        $('#result').append('<li class="link-class"><img src="' + value.image + '" height="40" width="40" class="img-thumbnail" /> ' + value.name + ' | <span class="text-muted">' + value.categories + '</span></a></li>');
-                    } else
-                        $.each(value.ingredients, function (k, v) {
-                            if (v.name.search(expression) !== -1) {
-                                $('#result').append('<li class="link-class"><img src="' + value.image + '" height="40" width="40" class="img-thumbnail" /> ' + value.name + ' | <span class="text-muted">' + value.categories + '</span></a></li>');
-                            } //<a class="userView" href="/recipe/' + encodeURIComponent(value.name) + '">
-                        });
-                });
-            });
-        });
+var app = new Vue({
+    el: '#root',
+    data:{
+        movies:[],
+        search: '',
+        mainSearch: '',
+        movieSearch: [],
+        showSearchList : true,
+        showmovie : false
+    },
+    methods:{
+        fetchPosts: function(){
+            this.showSearchList=true;
+            this.showmovie=false
+            axios.get("http://localhost:8080/title/?title=" + this.mainSearch).then(function(response){
+                this.movies = response.data;
+                console.log(mainSearch);
+                console.log(this.mainSearch);
 
-        $('#result').on('click', 'li', 'a', function () {
-            let click_text = $(this).text().split('|');
-            $('#userSearch').val($.trim(click_text[0]));
-            $("#result").html('');
-        });
 
-    });
-
-};*/
-
-function autocomplete(inp, arr) {
-    let currentFocus;
-    inp.addEventListener("input", function(e) {
-        let a, b, i, val = this.value;
-        closeAllLists();
-        if (!val) { return false;}
-        currentFocus = -1;
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        this.parentNode.appendChild(a);
-        for (i = 0; i < arr.length; i++) {
-            if (arr[i].substr(0, val.length).toUpperCase() === val.toUpperCase()) {
-                b = document.createElement("DIV");
-                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                b.innerHTML += arr[i].substr(val.length);
-                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-                b.addEventListener("click", function(e) {
-                    inp.value = this.getElementsByTagName("input")[0].value;
-                    closeAllLists();
-                });
-                a.appendChild(b);
-            }
-        }
-    });
-
-    inp.addEventListener("keydown", function(e) {
-        let x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode === 40) {
-            currentFocus++;
-            addActive(x);
-        } else if (e.keyCode === 38) {
-            currentFocus--;
-            addActive(x);
-        } else if (e.keyCode === 13) {
-            e.preventDefault();
-            if (currentFocus > -1) {
-                if (x) x[currentFocus].click();
-            }
-        }
-    });
-    function addActive(x) {
-        if (!x) return false;
-        removeActive(x);
-        if (currentFocus >= x.length) currentFocus = 0;
-        if (currentFocus < 0) currentFocus = (x.length - 1);
-        x[currentFocus].classList.add("autocomplete-active");
-    }
-    function removeActive(x) {
-        for (let i = 0; i < x.length; i++) {
-            x[i].classList.remove("autocomplete-active");
+            }.bind(this))
+        },
+        movieInfo: function(){
+            this.showSearchList=false;
+            this.showmovie=true
+            axios.get("http://localhost:8080/title/movie/?imdb=" + this.search).then(function(response){
+                this.movieSearch = response.data;
+                this.mainSearch = movieSearch.Title;
+            }.bind(this))
         }
     }
-    function closeAllLists(elmnt) {
-        let x = document.getElementsByClassName("autocomplete-items");
-        for (let i = 0; i < x.length; i++) {
-            if (elmnt !== x[i] && elmnt !== inp) {
-                x[i].parentNode.removeChild(x[i]);
-            }
-        }
-    }
-
-    document.addEventListener("click", function (e) {
-        closeAllLists(e.target);
-    });
-}
-
-let countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua & Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia & Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre & Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts & Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Turks & Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
-
-autocomplete(document.getElementById("userSearch"),countries);
-
+})
